8a2b6a750445078c457b13677420946f4884639b
JUANPER-GITHUB <80792459+JUANPER-GITHUB@users.noreply.github.com>
2021-03-24 22:31:20 -0500

    Add files via upload

---
+++ Punto 2.1_Lab22_2303_Ejemplo1.py
@@ -0,0 +1,3 @@
+for j in range(6):
+    for m in range(j):
+        print("i vale", j, "y j vale", m)
---
+++ Punto 2.1_Lab22_2303_Ejemplo2.py
@@ -0,0 +1,3 @@
+for s in range(3):
+    for o in range(s):
+        print("i vale", s, "y j vale", o)
